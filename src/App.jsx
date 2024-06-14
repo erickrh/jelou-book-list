c02ad5f2394f5590321627ef18b0dd85962cb474
Erick Riaño <123erickrh@gmail.com>
2024-06-13 21:36:08 -0500

    Install tailwindcss and redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import "./App.css";
-
 function App() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <h1 className="text-3xl font-bold underline">Welcome to Jelou Book</h1>
+      </header>
+    </>
+  );
 }
 
 export default App;
