b69310397a167b1c93e2932f242bc9bab0a0d630
Erick Riaño <123erickrh@gmail.com>
2024-06-14 10:30:24 -0500

    Clean code. Now grid is responsive

--- src/App.jsx
+++ src/App.jsx
@@ -27,26 +27,7 @@ function App() {
         </article>
 
         <article>
-          <div className='mt-5 grid grid-cols-3 gap-5'>
-            {/* <div className='flex h-72 w-[26rem] cursor-pointer flex-col items-center rounded-lg border border-gray-200 bg-white shadow hover:bg-gray-100 md:max-w-xl md:flex-row'>
-              <img
-                className='h-full w-1/2 rounded-none rounded-s-lg object-cover'
-                src={image}
-                alt=''
-              />
-              <div className='flex flex-col justify-between p-2 leading-normal'>
-                <h5 className='mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white'>
-                  Title
-                </h5>
-                <p className='mb-3 font-normal text-gray-700'>
-                  Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
-                  chronological order.
-                </p>
-                <p className='mb-3 text-sm font-normal text-gray-700'>Horror, Adventure</p>
-                <p className='mb-3 text-sm font-normal text-gray-700'>Author</p>
-              </div>
-            </div> */}
-
+          <div className='mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3'>
             <Card
               image={image}
               title={'Harry Potter'}
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -13,20 +13,5 @@ export default function Card({ image, title, synopsis, genre, author }) {
         <p className='mb-3 text-sm font-normal text-gray-700'>{author}</p>
       </div>
     </div>
-    // <div className='flex h-72 w-[26rem] cursor-pointer flex-col items-center rounded-lg border border-gray-200 bg-white shadow hover:bg-gray-100 md:max-w-xl md:flex-row'>
-    //   <img
-    //     className='h-full w-1/2 rounded-none rounded-s-lg object-cover'
-    //     src={image}
-    //     alt={title}
-    //   />
-    //   <div className='flex flex-col justify-between p-2 leading-normal'>
-    //     <h5 className='mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white'>
-    //       {title}
-    //     </h5>
-    //     <p className='mb-3 font-normal text-gray-700'>{synopsis}</p>
-    //     <p className='mb-3 text-sm font-normal text-gray-700'>{genre}</p>
-    //     <p className='mb-3 text-sm font-normal text-gray-700'>{author}</p>
-    //   </div>
-    // </div>
   );
 }
