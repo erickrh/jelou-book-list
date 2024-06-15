3da0b393e21f74fd4480ff382990c5b3921ba871
Erick Riaño <123erickrh@gmail.com>
2024-06-14 19:04:37 -0500

    Add hover animation in card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -17,22 +17,24 @@ export default function Card({ cover, title, synopsis, genre, author, isFavorite
   };
 
   return (
-    <div
-      className={`${isFavorite ? 'w-48 flex-none hover:w-96' : 'w-96'} group flex h-72 animate-fade-right cursor-pointer flex-col items-center rounded-lg border border-gray-200 bg-white shadow transition-all duration-200 ease-in animate-once animate-ease-in hover:bg-gray-100 md:max-w-xl md:flex-row`}
-      onClick={handleFavorite}
-    >
-      <img
-        className={`${isFavorite ? 'w-48 group-hover:w-1/2' : 'w-1/2'} h-full rounded-lg object-cover shadow-2xl transition-all duration-500 ease-in-out hover:rounded-s-lg`}
-        src={cover}
-        alt={title}
-      />
+    <div className={`${!isFavorite && 'p-3 transition-all duration-200 hover:-translate-y-4'}`}>
       <div
-        className={`${isFavorite ? 'hidden group-hover:flex' : ''} flex h-full w-full animate-fade-right flex-col justify-between p-2 leading-normal animate-once animate-ease-in-out`}
+        className={`${isFavorite ? 'w-48 flex-none hover:w-96' : 'w-96 hover:h-80 hover:w-[26rem]'} group flex h-72 animate-fade-right cursor-pointer flex-col items-center rounded-lg border border-gray-200 bg-white shadow transition-all duration-200 ease-in animate-once animate-ease-in hover:bg-gray-100 md:max-w-xl md:flex-row`}
+        onClick={handleFavorite}
       >
-        <h5 className='mb-2 text-xl font-bold tracking-tight text-gray-900'>{title}</h5>
-        <p className='mb-3 whitespace-normal font-normal text-gray-700'>{synopsis}</p>
-        <p className='mb-3 text-sm font-normal text-gray-700'>{genre}</p>
-        <p className='mb-3 text-sm font-normal text-gray-700'>{author}</p>
+        <img
+          className={`${isFavorite ? 'w-48 group-hover:w-1/2' : 'w-1/2'} h-full rounded-lg object-cover shadow-2xl transition-all duration-500 ease-in-out hover:rounded-s-lg`}
+          src={cover}
+          alt={title}
+        />
+        <div
+          className={`${isFavorite && 'hidden group-hover:flex'} flex h-full w-full animate-fade-right flex-col justify-between p-2 leading-normal animate-once animate-ease-in-out`}
+        >
+          <h5 className='mb-2 text-xl font-bold tracking-tight text-gray-900'>{title}</h5>
+          <p className='mb-3 whitespace-normal font-normal text-gray-700'>{synopsis}</p>
+          <p className='mb-3 text-sm font-normal text-gray-700'>{genre}</p>
+          <p className='mb-3 text-sm font-normal text-gray-700'>{author}</p>
+        </div>
       </div>
     </div>
   );
